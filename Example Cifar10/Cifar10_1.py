74c13ffa6ddc9f983ebabc4f5b018a2df48ccfb7
JChann <79005063+chaaaaaaaaaaan@users.noreply.github.com>
2021-06-16 15:19:18 +0900

    Add files via upload

---
+++ Example Cifar10/Cifar10_1.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 09:28:03 2021
+
+@author: skukm
+"""
+
+#import library
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout, AveragePooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+
+#def plot
+def plot_loss(history):
+    plt.figure()
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title("Model loss")
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.legend(['Train', 'val'], loc=0)
+    plt.show()
+
+
+def plot_accuracy(history):
+    plt.figure()
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title("Model accuracy")
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend(['Train', 'val'], loc=0)
+    plt.show()
+    
+#load data
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype("float32")/255
+x_test = x_test.astype("float32")/255
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=18)
+
+#model
+model = Sequential()
+model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu',
+                 kernel_initializer='he_normal', input_shape=(32, 32, 3)))
+model.add(AveragePooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding='same', activation='relu'))
+model.add(AveragePooling2D(pool_size=(2,2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(filters=256, kernel_size=(2,2), padding='same', activation='relu'))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters=64, kernel_size=(2,2), padding='same', activation='relu'))
+model.add(Dropout(0.5))
+model.add(Flatten())
+
+model.add(Dense(128, activation = 'relu'))
+model.add(Dense(10, activation = 'softmax'))
+model.summary()
+
+#model compile, evaluate
+model.compile(optimizer='adamax', loss='categorical_crossentropy', metrics='accuracy')
+
+# history = model.fit(x_train, y_train, batch_size=64, 
+#                     epochs=15, verbose=1, validation_split=0.2)
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.0001)
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+
+datagen.fit(x_train)
+
+batch_size= 128
+
+# Fit the model
+history = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs=100, validation_data = (x_val, y_val),
+                              verbose = 1, steps_per_epoch=x_train.shape[0] // batch_size
+                              , callbacks=[learning_rate_reduction]) 
+                              
+score = model.evaluate(x_test, y_test, verbose=1)
+print("Test loss: ", score[0])
+print("Test acc:", score[1])
+
+plot_loss(history)
+plot_accuracy(history)
+
+#print image
+classes=np.array(['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'])
+
+pred = model.predict(x_test)
+pred_single = classes[np.argmax(pred,axis=-1)]
+acutal_single = classes[np.argmax(y_test,axis=-1)]
+
+n_to_show = 10
+indices = np.random.choice(range(len(x_test)), n_to_show)
+
+fig = plt.figure(figsize=(15,3))
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+for i, idx in enumerate(indices):
+    img = x_test[idx]
+    ax = fig.add_subplot(1, n_to_show, i+1)
+    ax.axis('off')
+    ax.text(0.5, -0.35, 'pred=' + str(pred_single[idx]), fontsize=10,
+            ha='center', transform=ax.transAxes)
+    ax.text(0.5, -0.7, 'act=' + str(acutal_single[idx]), fontsize=10,
+        ha='center', transform=ax.transAxes)
+    ax.imshow(img)
+    
+plt.show()
